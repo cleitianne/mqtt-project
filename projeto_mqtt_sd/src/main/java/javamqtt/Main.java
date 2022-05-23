97dadf60ee8364cf3ebb30554bcdff93fcdc829c
cleitianne <oliveira.cleiti@gmail.com>
2022-05-23 11:18:41 -0300

    Ajustando tempo para 60 segundos

--- projeto_mqtt_sd/src/main/java/javamqtt/Main.java
+++ projeto_mqtt_sd/src/main/java/javamqtt/Main.java
@@ -8,6 +8,7 @@ public class Main {
     private final static int maxTemp = 2000;
 
     public static void main(String[] args) throws InterruptedException {
+        System.out.println("Bem vindo ao projeto de captura de temperatura e envio de informações!!");
         TemperatureSensor temperatureSensor = new TemperatureSensor("tcp://localhost:1883", null, null);
         temperatureSensor.start();
 
@@ -15,7 +16,7 @@ public class Main {
             String temperatura = Integer.toString(ThreadLocalRandom.current().nextInt(minTemp, maxTemp + 1));
             System.out.println("Current Temperature: "+ temperatura);
             temperatureSensor.publish("sensor/temperature/caldeira", temperatura.getBytes(), 0);
-            Thread.sleep(10000);
+            Thread.sleep(60000);
         }
 
     }
